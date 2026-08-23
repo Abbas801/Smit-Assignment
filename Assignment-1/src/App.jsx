2b72107c5909461bef4a0dd2fef3dcac82d0f0cc
muhammad abbas <abbas80010@gmail.com>
2026-08-23 15:06:10 +0500

    Initial assignment project

---
+++ Assignment-1/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React Page</h1>
+
+      <h2>Muhammad Abbas</h2>
+
+      <p>
+        I am a Software Engineering student learning React and JavaScript.
+      </p>
+
+      <h3>My Skills</h3>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default App;
